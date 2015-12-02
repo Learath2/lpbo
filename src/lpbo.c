6cfb3d7f89a9ec093b4c9a6bb88296bf4efb9357
Learath2 <learath2@gmail.com>
2015-12-02 15:54:23 +0200

    Initial commit.

---
+++ src/lpbo.c
@@ -0,0 +1,213 @@
+/* lpbo.c - (C) 2015, Emir Marincic
+ * lpbo - A tool to work with Arma pbo files.
+ * See README for contact-, COPYING for license information. */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include <getopt.h>
+
+#ifdef HAVE_DIRECT_H
+    #include <direct.h>
+#endif
+
+#include <libpbo/pbo.h>
+
+enum mode
+{
+	UNKNOWN = 0,	//None of the following
+	LIST,			// -l
+	EXTRACT,		// -x
+	CREATE			// -c
+};
+
+enum mode g_mode;
+const char *g_program_name;
+const char *g_file;
+const char *g_dir;
+
+#define USAGE_E(STR)\
+				fputs(STR "\n", stderr); \
+				usage(EXIT_FAILURE);
+
+void usage(int ec);
+
+void create_directories(char *path)
+{    
+    for(int i = 0; path[i] != '\0'; i++) {
+        if(path[i] == '/') {
+            path[i] = '\0';
+            #ifdef HAVE_DIRECT_H
+                mkdir(path);
+            #else
+                mkdir(path, 0755);
+            #endif
+            path[i] = '/';
+        }
+    }
+}
+
+void set_mode(enum mode mode)
+{
+	if(g_mode != UNKNOWN){
+		USAGE_E("You may not specify more then one '-xcl' option.")
+	}
+	g_mode = mode;
+}
+
+void set_file(const char *file)
+{
+	if(g_file){
+		USAGE_E("You may not specify multiple files.")
+	}
+	g_file = file;
+}
+
+void usage(int ec)
+{
+	FILE *str = ec == EXIT_FAILURE ? stderr : stdout;
+	#define O(STR) fputs(STR "\n", str)
+	#define F(STR, ...) fprintf(str, STR "\n", __VA_ARGS__)
+	
+	if(ec == EXIT_FAILURE){
+		F("Try '%s -h' for more information.", g_program_name);
+		exit(ec);
+	}
+
+	O("=============================");
+	O("== lpbo - An Arma Pbo editor ==");
+	O("=============================");
+	F("usage: %s [-lxcfCh] [FILE]...", g_program_name);
+	O("");
+	O("\t-l : List contents of file.");
+	O("\t-x : Extract contents of file.");
+	O("\t-c : Create a new pbo.");
+	O("\t-f <str>: Use pbo file.");
+	O("\t-C <str>: Change to directory.");
+	O("\t-h : Display this.");
+	O("");
+	O("(C) 2015 Emir Marincic");
+
+	#undef O
+	#undef F
+}
+
+void process_args(int *argc, char ***argv)
+{
+	int c = 0;
+	while((c = getopt(*argc, *argv, "lxcf:C:h")) != -1)
+	{
+		switch(c) {
+		case 'l':
+			set_mode(LIST);
+			break;
+		case 'x':
+			set_mode(EXTRACT);
+			break;
+		case 'c':
+			set_mode(CREATE);
+			break;
+		case 'f':
+			set_file(optarg);
+			break;
+		case 'C':
+			g_dir = optarg;
+			break;
+		case 'h':
+			usage(EXIT_SUCCESS);
+			break;
+		case '?':
+		default:
+			USAGE_E("Error")
+		}
+	}
+	*argc -= optind;
+	*argv += optind;
+}
+
+void list_files_cb(const char *file, void *user)
+{
+	if(file[0] == '\0')
+		return;
+	printf("%s\n", file);
+}
+
+void list_files()
+{
+	pbo_t d = pbo_init(g_file);
+	pbo_read_header(d);
+	pbo_get_file_list(d, list_files_cb, NULL);
+}
+
+void extract_files_cb(const char *filename, void *user)
+{
+	if(filename[0] == '\0')
+		return;
+
+	pbo_t d = (pbo_t)user;
+
+	char buf[512];
+	if(g_dir){
+		strcpy(buf, g_dir);
+		strcat(buf, "/");
+		strcat(buf, filename);
+	}
+	else
+		strcpy(buf, filename);
+
+	for(int i = 0; buf[i] != '\0'; i++)
+		if(buf[i] == '\\')
+			buf[i] = '/';
+
+	create_directories(buf);
+
+	FILE *file = fopen(buf, "w");
+	if(!file)
+		return;
+	pbo_write_to_file(d, filename, file);
+	fclose(file);
+}
+
+void extract_files()
+{
+	pbo_t d = pbo_init(g_file);
+	pbo_read_header(d);
+	pbo_get_file_list(d, extract_files_cb, d);
+}
+
+void create_pbo()
+{
+	printf("Not implemented.\n");
+}
+
+int main(int argc, char **argv)
+{
+	g_mode = UNKNOWN;
+	g_program_name = argv[0];
+
+	process_args(&argc, &argv);
+
+	switch(g_mode) {
+	case UNKNOWN:
+		USAGE_E("You must specify one of the '-xcl' options")
+		break;
+	case LIST:
+		list_files();
+		break;
+	case EXTRACT:
+		extract_files();
+		break;
+	case CREATE:
+		create_pbo();
+		break;
+	}
+
+	exit(EXIT_SUCCESS);
+}
